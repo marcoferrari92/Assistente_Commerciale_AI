c95dd00e1a46dd6044eddecd44b5b2d1f4e7e004
marcoferrari92 <74426411+marcoferrari92@users.noreply.github.com>
2026-05-25 16:48:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -760,7 +760,7 @@ if utente_connesso:
                             messaggio_personalizzato=st.session_state.messaggio_email_personalizzato,
                             file_caricati=uploaded_files
                         )
-                        st.write(f"Risposta finale della funzione di invio: {'E-mail partita' if email_ok else 'Errore durante l'esecuzione del codice d'invio'}")
+                        st.write(f"Risposta finale della funzione di invio: {'E-mail partita' if email_ok else 'Errore durante l\'esecuzione del codice d\'invio'}")
         else:
             st.warning("⚠️ L'invio e-mail automatico non è attivo o manca l'indirizzo del collega.")
         
